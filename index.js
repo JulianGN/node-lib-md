435650b9c96b70971383b56b958e7a3df9597cf6
Julian Nunes <contato@juliannunes.com.br>
2023-02-17 23:10:06 -0300

    Coletando os links com regex

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
-console.log('olá mundo');
+import fs from "fs"; // biblioteca para ler arquivos e diretórios
+import chalk from "chalk"; // estilizar o console
+
+function extrairLinks(texto) {
+  const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
+  const capturas = [...texto.matchAll(regex)]; // o matchAll retorna uma array das capturas, mas se não utilizar o spread, apenas o primeiro item vai aparecer
+
+  const resultados = capturas.map((c) => ({
+    [c[1]]: c[2], // Desse modo retorna em cada objeto o nome da chave como o nome da chave do segundo item de cada objeto da array de capturas e o valor é o terceiro item de cada objeto do array de capturas
+  }));
+
+  return resultados;
+}
+
+// function tratarErro(erro) {
+//   throw new Error(chalk.red(erro.code, "Arquivo não encontrado"));
+// }
+
+// function pegarArquivo(caminhoDoArquivo) {
+//   const encoding = "utf-8";
+//   fs.promises
+//     .readFile(caminhoDoArquivo, encoding)
+//     .then((texto) => console.log(chalk.green(texto)))
+//     .catch((erro) => tratarErro(erro));
+// }
+
+async function pegarArquivo(caminhoDoArquivo) {
+  try {
+    const encoding = "utf-8";
+    const texto = await fs.promises.readFile(caminhoDoArquivo, encoding);
+    console.log(extrairLinks(texto));
+  } catch (erro) {
+    tratarErro(erro);
+  }
+}
+
+// \[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)
+// Usar class [] para selecionar uma sequência de caracteres definida
+// ^ para negar um caractere
+// \ para escapar, possibilitando usar caracteres reservados
+// *? para definir que pode repetir
+// () para separar grupos
+// ? para dizer que o caractere é opcional
+
+// pegarArquivo("./arquivos/texto.md");
+// pegarArquivo("./arquivos/texto.m"); //testar o erro
+
+pegarArquivo("./arquivos/texto.md");
